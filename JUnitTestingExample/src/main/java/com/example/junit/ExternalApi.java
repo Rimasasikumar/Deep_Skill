4aa61bda4de1cab6b6950742cb647fb43448e9b9
Rimasasikumar <rimasasikumar@gmail.com>
2026-07-22 22:44:13 +0530

    Ignore Maven build artifacts

---
+++ JUnitTestingExample/src/main/java/com/example/junit/ExternalApi.java
@@ -0,0 +1,7 @@
+package com.example.junit;
+
+public interface ExternalApi {
+
+    String getData();
+
+}
---
+++ JUnitTestingExample/src/main/java/com/example/junit/MyService.java
@@ -0,0 +1,15 @@
+package com.example.junit;
+
+public class MyService {
+
+    private ExternalApi externalApi;
+
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+
+    public String fetchData() {
+        return externalApi.getData();
+    }
+
+}
