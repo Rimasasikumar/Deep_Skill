2dd3dc8015baf907590338acd19b035d29d55f22
Rimasasikumar <rimasasikumar@gmail.com>
2026-07-23 19:29:43 +0530

    Added REST endpoint to retrieve country by code

--- spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
+++ spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -1,14 +1,53 @@
+
+// package com.cognizant.spring_learn.controller;
+
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+
+// import org.springframework.context.ApplicationContext;
+// import org.springframework.context.support.ClassPathXmlApplicationContext;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.cognizant.spring_learn.Country;
+
+// @RestController
+// public class CountryController {
+
+//     private static final Logger LOGGER =
+//             LoggerFactory.getLogger(CountryController.class);
+
+//     @RequestMapping("/country")
+//     public Country getCountryIndia() {
+
+//         LOGGER.info("START - getCountryIndia()");
+
+//         ApplicationContext context =
+//                 new ClassPathXmlApplicationContext("country.xml");
+
+//         Country country = context.getBean("country", Country.class);
+
+//         ((ClassPathXmlApplicationContext) context).close();
+
+//         LOGGER.info("END - getCountryIndia()");
+
+//         return country;
+//     }
+// }
+
+//Exercise 2
 package com.cognizant.spring_learn.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.spring_learn.Country;
+import com.cognizant.spring_learn.service.CountryService;
 
 @RestController
 public class CountryController {
@@ -16,19 +55,17 @@ public class CountryController {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(CountryController.class);
 
-    @RequestMapping("/country")
-    public Country getCountryIndia() {
+    @Autowired
+    private CountryService countryService;
 
-        LOGGER.info("START - getCountryIndia()");
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
 
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("country.xml");
+        LOGGER.info("START - getCountry()");
 
-        Country country = context.getBean("country", Country.class);
+        Country country = countryService.getCountry(code);
 
-        ((ClassPathXmlApplicationContext) context).close();
-
-        LOGGER.info("END - getCountryIndia()");
+        LOGGER.info("END - getCountry()");
 
         return country;
     }
