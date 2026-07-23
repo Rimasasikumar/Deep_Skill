3e489dcd1a1c484690327dd3300ce30d09b2a348
Rimasasikumar <rimasasikumar@gmail.com>
2026-07-23 21:21:37 +0530

    MicroServices handson implemented

---
+++ micro_services/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,35 @@
+package com.cognizant.loan.controller;
+
+import com.cognizant.loan.model.Loan;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(LoanController.class);
+
+    @GetMapping("/loans/{number}")
+    public Loan getLoan(@PathVariable String number) {
+
+        LOGGER.info("START");
+
+        Loan loan = new Loan(
+                number,
+                "car",
+                400000,
+                3258,
+                18);
+
+        LOGGER.info("END");
+
+        return loan;
+    }
+
+}
